a94b2c9ff9279a1c69bee78d140830851bb2f3fc
Satheesh <satheeshkumar.s@platform3solutions.com>
2020-07-09 14:59:29 +0530

    enabling CORS

--- driverless/src/main/java/com/upgrad/hirewheels/controller/AuthenticationController.java
+++ driverless/src/main/java/com/upgrad/hirewheels/controller/AuthenticationController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class AuthenticationController{
 
--- driverless/src/main/java/com/upgrad/hirewheels/controller/VehicleController.java
+++ driverless/src/main/java/com/upgrad/hirewheels/controller/VehicleController.java
@@ -17,6 +17,7 @@ import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class VehicleController {
 
